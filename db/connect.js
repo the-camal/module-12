803abc78346e3c54f2a707a1ee532d1f0bbdbe23
paulcourts <hubertcourts@gmail.com>
2023-01-24 11:45:33 -0600

    getting done with my connections and working on index.js inside my db.

--- db/connect.js
+++ db/connect.js
@@ -1,1 +1,13 @@
+const mysql = require("mysql2");
+const connection = mysql.createconnection({
+    host: "localhost",
+    user: "",
+    password: "",
+    database: "worker"
+});
 
+connection.connect(function (err) {
+    if (err) throw err;
+});
+
+module.exports = connection;
